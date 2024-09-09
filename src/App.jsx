d31d98e9290ef18e319d58eab956303d1b03ebcf
Omar ♔ <o_yousaf@live.co.uk>
2024-09-09 11:02:37 +0100

    implementing scrollToTop

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,30 @@ import Hero from "./components/Hero";
 import Values from "./components/Values";
 import Volunteer from "./components/Volunteer";
 
+import { useEffect, useState } from "react";
+import { animateScroll as scroll } from "react-scroll";
+import { FaArrowUp } from "react-icons/fa";
+
 const App = () => {
+  const [showScroll, setShowScroll] = useState(false);
+  const [hover, setHover] = useState(false);
+
+  const checkScrollTop = () => {
+    if (!showScroll && window.pageYOffset > 500) {
+      setShowScroll(true);
+    } else if (showScroll && window.pageYOffset <= 500) {
+      setShowScroll(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", checkScrollTop);
+    return () => window.removeEventListener("scroll", checkScrollTop);
+  }, [showScroll]);
+
+  const scrollToTop = () => {
+    scroll.scrollToTop({ duration: 300, smooth: "easeInOutQuad" });
+  };
   return (
     <>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
@@ -18,8 +41,32 @@ const App = () => {
         <Footer />
       </div>
       <ButtonGradient />
+      {showScroll && (
+        <div
+          onClick={scrollToTop}
+          onMouseEnter={() => setHover(true)}
+          onMouseLeave={() => setHover(false)}
+          style={{
+            position: "fixed",
+            bottom: "90px",
+            right: "50px",
+            cursor: "pointer",
+            background: hover ? "#ED872D" : "#C34A2C",
+            transition: "background 0.3s ease",
+            color: hover? "#005989" : "#fff",
+            borderRadius: "100%",
+            padding: "15px",
+            zIndex: 1000,
+            fontSize: "24px",
+            
+          }}
+        >
+          <FaArrowUp />
+        </div>
+      )}
     </>
-  );``
+  );
+  ``;
 };
 
 export default App;
