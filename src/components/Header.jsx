5ed03b28c644afe9d2b83efe85dd41c80432bcf8
Omar ♔ <o_yousaf@live.co.uk>
2024-09-17 12:56:09 +0100

    squashing bugs

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -52,7 +52,7 @@ const Header = () => {
             openNavigation ? "flex" : "hidden"
           } fixed z-10 top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}
         >
-          <div className="relative flex flex-col items-center justify-center m-auto lg:flex-row font-bold">
+          <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row font-bold">
             {navigation.map((item) => (
               <Link
                 key={item.id}
